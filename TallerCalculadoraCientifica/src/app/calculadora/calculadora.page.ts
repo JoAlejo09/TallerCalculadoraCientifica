77cc471c9d88d518f12733526625a3c3f0f060a6
Jose Pila <joselejandro_pilavizuete@hotmail.com>
2025-10-07 17:32:16 -0500

    UPDATE CALCULADORA

--- TallerCalculadoraCientifica/src/app/calculadora/calculadora.page.ts
+++ TallerCalculadoraCientifica/src/app/calculadora/calculadora.page.ts
@@ -1,20 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'app-calculadora',
+  selector: 'app-home',
+  standalone: true,
   templateUrl: './calculadora.page.html',
   styleUrls: ['./calculadora.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class CalculadoraPage implements OnInit {
+export class CalculadoraPage {
+  
+ 
+  num1: string = '';
+  num2: string = '';
+  result: number | null = null;
+  operation: string = 'Resultado';
+ 
+  calculate(op: 'sum' | 'sub'| 'mul' | 'div' | 'sqr' | 'pow2' | 'pow3' | 'sin' | 'cos' |'tan'|'log2'|'ln') {
+    const a = Number(this.num1);
+    const b = Number(this.num2);
 
-  constructor() { }
+    if (isNaN(a) || isNaN(b)) {
+      this.result = null;
+      this.operation = 'Error: valores inválidos';
+      return;
+    }
 
-  ngOnInit() {
-  }
+    if (op === 'sum') {
+      this.result = a + b;
+      this.operation = 'Suma';
+    } else if(op ==='sub') {
+      this.result = a - b;
+      this.operation = 'Resta';
+    } else if(op === 'mul'){
+      this.result = a * b;
+      this.operation = 'Producto'
+    } else if(op ==='div'){
+        if(b===0){
+          this.operation = 'Error: valores inválidos';
+          this.result=0;
+        }
+        this.result = a/b;
+    } else if(op ==='sqr'){
+        if(a<0){
+          this.operation = 'Error: valores inválidos';
+          this.result=0;
+        }
+        this.result = Math.sqrt(a);
 
-}
+    } else if(op =='pow2'){
+      this.result = Math.pow(a,2);
+    } else if(op = 'pow3'){
+      this.result = Math.pow(a,3);
+    } else if(op='sin'){
+      this.result = Math.sin(a); 
+    } else if(op='cos'){
+      this.result = Math.cos(a); 
+    } else if(op='tan'){
+      this.result = Math.tan(a); 
+    } else if(op='log2'){
+      if(a<1){
+          this.operation = 'Error: valores inválidos';
+          this.result=0; 
+      }
+      this.result = Math.log2(a);
+    } else if(op='ln'){
+      if(a<1){
+          this.operation = 'Error: valores inválidos';
+          this.result=0;    
+      }
+      this.result = Math.log(a);
+    }
+  }}
